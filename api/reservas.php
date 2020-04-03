b4f1d9574e992c2dc8a0243f93ecf6d9a4f18aa6
Oroel Ipas <oroel>
2020-04-03 20:21:17 +0200

    ahora api/misreservas devuleve solo las reservas futuras

--- api/reservas.php
+++ api/reservas.php
@@ -1,8 +1,8 @@
 <?php
 
 
-function diaSemana($i){
-	switch ($i) {
+function diaSemana($dia){
+	switch ($dia) {
 		case '1':
 			return 'L';
 			break;
@@ -19,7 +19,7 @@ function diaSemana($i){
 			return 'V';
 			break;
 		default:
-			echo "ERROR: actividad en diaSemana=". $i;
+			echo "ERROR: actividad en diaSemana=". $dia;
 			break;
 	}
 }
@@ -116,15 +116,54 @@ function misReservas(){
 			  WHERE r.idusuario = ? and 
 			  		r.idactividad = a.idactividad and
 			  		r.hora = a.hora and
-			  		r.diasemana = a.diasemana";
+			  		r.diasemana = a.diasemana and 
+			  		NOW() < CONCAT(r.fecha, ' ', r.hora)";
+
+
+	/// APAÑO PARA QUE SIMULE ESTAR SIEMPRE EN LA UNICA SEMANA EN LA QUE TENEMOS DATOS Y RESERVAS: 03/02/2020 - 07-02-2020
+
+
+	$diaSemana = date("w");
+	// $diaFake = dia de febrero en el que simularemos estar
+	switch ($diaSemana) {
+		case '1':
+			$diaFake =  '2020-02-03';
+			break;
+		case '2':
+			$diaFake = '2020-02-04';
+			break;
+		case '3':
+			$diaFake = '2020-02-05';
+			break;
+		case '4':
+			$diaFake = '2020-02-06';
+			break;
+		case '5':
+			$diaFake = '2020-02-07';
+			break;
+		case '6':
+		case '7':
+			$diaFake = '2020-02-02'; // para que si estamos en fin de semana nos devuelva todas las de la semana siguiente
+			break;
+	}
+
+	$dateFake = $diaFake ." ". date("H:i:s");
+
+	$query = "SELECT r.idReserva, a.idactividad, a.hora, a.diasemana, r.fecha, a.nombre  
+			  FROM reservas r, actividades a 
+			  WHERE r.idusuario = ? and 
+			  		r.idactividad = a.idactividad and
+			  		r.hora = a.hora and
+			  		r.diasemana = a.diasemana and 
+			  		'".$dateFake."' < CONCAT(r.fecha, ' ', r.hora)";
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 	$result = $db->executeSql($query, [$idUser]);
 	echo json_encode($result,  JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 	return;
 
 }
-
-
-
 
 ?>
